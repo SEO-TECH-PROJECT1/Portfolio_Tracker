76cb5960a2a1e40d85324071985d8419849b0a2a
BRIAN BAZURTO <124537281+ba-00001@users.noreply.github.com>
2024-07-16 21:52:21 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+# File: Portfolio_Tracker-main/config.py
+# Description: Configuration file for the Flask application.
+
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from a .env file
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///stock_portfolio.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    ALPHA_VANTAGE_API_KEY = os.environ.get('ALPHA_VANTAGE_API_KEY')
+    FMP_API_KEY = os.environ.get('FMP_API_KEY')
