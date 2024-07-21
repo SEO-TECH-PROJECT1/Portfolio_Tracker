5068742274c2770bab5cfe4c33c7c1ed34b71ac0
Gracious Ogyiri Asare <gpoa@bu.edu>
2024-07-20 22:29:00 -0500

    setup for autodeployment

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,19 @@
 # Portfolio_Tracker/wsgi.py
 
 
-from app import create_app
+import git
+from flask import request
+from app import app, create_app
 
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/PortfolioTracker/Portfolio_Tracker')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
+    
 app = create_app()
+
